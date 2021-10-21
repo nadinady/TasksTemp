a98694cf3ebb8dcbeb755c2f69a8db90353dd847
Nad K <nadinka121@rambler.ru>
2021-10-21 15:50:31 +0300

    Created Task5

--- Task/src/main/java/1/App.java
+++ Task/src/main/java/1/App.java
@@ -1,13 +1,61 @@
-package 1;
+//package 1;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        String MonthNumber;
+        Scanner in = new Scanner(System.in);
+        System.out.println( "Please enter the number from 1 to 12" );
+        MonthNumber=in.next();
+        PrintMonthName(MonthNumber);
+
+
+    }
+    public static void PrintMonthName (String MonthNumber){
+            switch (MonthNumber) {
+                case "1":
+                    System.out.println("January");
+                    break;
+                case "2":
+                    System.out.println("February");
+                    break;
+                case "3":
+                    System.out.println("March");
+                    break;
+                case "4":
+                    System.out.println("April");
+                    break;
+                case "5":
+                    System.out.println("May");
+                    break;
+                case "6":
+                    System.out.println("June");
+                    break;
+                case "7":
+                    System.out.println("July");
+                    break;
+                case "8":
+                    System.out.println("August");
+                    break;
+                case "9":
+                    System.out.println("September");
+                    break;
+                case "10":
+                    System.out.println("October");
+                    break;
+                case "11":
+                    System.out.println("November");
+                    break;
+                case "12":
+                    System.out.println("December");
+                    break;
+                default:System.out.println("Incorrect number");
+            }
+
+
+
     }
 }
